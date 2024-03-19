831b9e62f549908de664dbb8a3d37e707678ebf6
Enmanuel Perez <Enmanuel_21_@hotmail.com>
2024-03-19 18:50:49 -0400

    commit

---
+++ Proyecto ISR/Controller/CalculoController.cs
@@ -0,0 +1,67 @@
+﻿namespace Proyecto_ISR.Controller
+{
+    public class CalculoController
+    {
+        private readonly List<RangoSueldoTipo> listaRangoSalarial = new()
+        {
+            new RangoSueldoTipo
+            {
+                Tipo = 1, //Exento
+                RangoAnual = 416220
+            },
+
+            new RangoSueldoTipo
+            {
+                Tipo = 2, //15%
+                RangoAnual = 624329
+            },
+
+            new RangoSueldoTipo
+            {
+                Tipo = 3, //20%
+                RangoAnual = 867123
+            },
+
+            new RangoSueldoTipo
+            {
+                Tipo = 4, //25%
+                RangoAnual = 867123.01m
+            }
+        };
+
+        public short evaluarSueldo(decimal sueldoActual)
+        {
+            short tipoRango;
+            try
+			{
+				decimal sueldoAnual = sueldoActual * 12;
+                tipoRango = listaRangoSalarial.Where(x => x.RangoAnual >= sueldoActual).Max(x => x.Tipo);
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+
+            return tipoRango;
+        }
+
+        public decimal evaluarSueldo15(decimal sueldoMensual)
+        {
+            return 0;
+        }
+        public decimal evaluarSueldo20(decimal sueldoMensual)
+        {
+            return 0;
+        }
+        public decimal evaluarSueldo25(decimal sueldoMensual)
+        {
+            return 0;
+        }
+    }
+
+	internal class RangoSueldoTipo
+    {
+        public decimal RangoAnual { get; set; }
+        public short Tipo { get; set; }
+    }
+}
--- Proyecto ISR/Program.cs
+++ Proyecto ISR/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Proyecto_ISR.Controller;
+using System;
+
+namespace Proyecto_ISR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool menuActivo = true;
+
+            CalculoController controller = new CalculoController();
+
+            while (menuActivo)
+            {
+                Console.Clear();
+                Console.WriteLine("Bienvenido al Calculo del ISR");
+                Console.WriteLine("\n\nFavor ingresar su sueldo mensual: ");
+
+                string entrada = Console.ReadLine() ?? "";
+
+                if (decimal.TryParse(entrada, out decimal sueldoActual))
+                {
+                    decimal isrPagar = 0;
+
+                    short opcionEvaluacion = controller.evaluarSueldo(sueldoActual);
+
+                    switch (opcionEvaluacion)
+                    {
+                        case 1: //Exento
+                            break;
+                        case 2: //15%
+                            controller.evaluarSueldo15(sueldoActual);
+                            break;
+                        case 3: //20%
+                            controller.evaluarSueldo20(sueldoActual);
+                            break;
+                        case 4: //25%
+                            controller.evaluarSueldo25(sueldoActual);
+                            break;
+                        default:
+                            isrPagar = 0;
+                            break;
+                    }
+
+                    Console.WriteLine($"El ISR a pagar es de {isrPagar} mensual");
+                }
+                else
+                {
+                    Console.WriteLine("Favor ingresar solo valores numericos");
+                    Console.ReadKey();
+                }
+
+            }
+        }
+    }
+}
